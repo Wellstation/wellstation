df33a4f7752661bd9bf8afeecee9502b4e4a66cb
Wellstation <prh0917@gmail.com>
2025-07-30 20:31:08 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   darkMode: 'media',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'hover:scale-105',
+    'hover:opacity-90',
+    'transition',
+    'duration-300',
+    'text-white',
+    'rounded-2xl',
+    'shadow-xl',
+    'font-semibold',
+    'bg-[#1C3058]',
+    'bg-[#8444dd]',
+    'bg-[#a52c1e]',
   ],
   theme: {
     extend: {},
