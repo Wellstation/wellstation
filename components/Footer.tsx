26d0d714d4d3be97f4a190e3e0b6e3ecff437f20
Wellstation <prh0917@gmail.com>
2025-07-30 20:33:50 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,35 @@
-<footer className="bg-gray-900 text-white text-sm px-4 py-6 mt-10 w-full">
-  <div className="max-w-screen-md mx-auto space-y-4 text-center leading-relaxed">
-    {/* 1열 */}
-    <div>
-      <p className="font-semibold">신흥모터스 & 앱스 대표 : 박경남</p>
-      <p>주소 : 부산광역시 강서구 대저중앙로 383-1</p>
-    </div>
-    {/* 2열 */}
-    <div>
-      <p>사업자등록번호 : 859-37-01635</p>
-      <p>호스팅서비스사업자 : Vercel Inc.</p>
-    </div>
-    {/* 3열 */}
-    <div>
-      <p>이메일 : <a href="mailto:apsauto@naver.com" className="underline text-blue-300">apsauto@naver.com</a></p>
-      <p>고객센터 : <a href="tel:16684509" className="text-blue-400 font-semibold">1668-4509</a></p>
-    </div>
-  </div>
-</footer>
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-sm px-4 py-6 mt-10 w-full">
+      <div className="max-w-screen-md mx-auto space-y-4 text-center leading-relaxed">
+        {/* 1열 */}
+        <div>
+          <p className="font-semibold">신흥모터스 & 앱스 대표 : 박경남</p>
+          <p>주소 : 부산광역시 강서구 대저중앙로 383-1</p>
+        </div>
+
+        {/* 2열 */}
+        <div>
+          <p>사업자등록번호 : 859-37-01635</p>
+          <p>호스팅서비스사업자 : Vercel Inc.</p>
+        </div>
+
+        {/* 3열 */}
+        <div>
+          <p>
+            이메일 :
+            <a href="mailto:apsauto@naver.com" className="underline text-blue-300">
+              apsauto@naver.com
+            </a>
+          </p>
+          <p>
+            고객센터 :
+            <a href="tel:16684590" className="text-blue-400 font-semibold">
+              1668-4590
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
