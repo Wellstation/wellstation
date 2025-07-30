54bec34553efa86c8fac2737c61f628e8b72043e
Wellstation <prh0917@gmail.com>
2025-07-30 20:34:56 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black px-4 py-10">
+    <div className="min-h-screen flex flex-col items-center justify-between bg-white dark:bg-black px-4 py-10">
+      
       {/* 로고 */}
       <header className="mb-10">
         <Image src="/logo1.svg" alt="로고" width={250} height={110} />
@@ -15,33 +16,27 @@ export default function Home() {
       <main className="flex flex-col items-center gap-6 w-full max-w-xs">
         {/* 정비 예약 */}
         <Link href="/reserve/repair">
-          <button
-            className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
-                       bg-[#2C3E50] dark:bg-[#34495E] dark:border-gray-600 border
-                       hover:opacity-90 hover:scale-105 transition duration-300"
-          >
+          <button className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
+                            bg-[#1C3058] dark:bg-[#1C3058] dark:border-gray-600 border
+                            hover:opacity-90 hover:scale-105 transition duration-300">
             정비 예약
           </button>
         </Link>
 
         {/* 튜닝 예약 */}
         <Link href="/reserve/tuning">
-          <button
-            className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
-                       bg-[#E8444D] dark:bg-[#A569BD] dark:border-purple-300 border
-                       hover:opacity-90 hover:scale-105 transition duration-300"
-          >
+          <button className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
+                            bg-[#8444dd] dark:bg-[#A569BD] dark:border-purple-300 border
+                            hover:opacity-90 hover:scale-105 transition duration-300">
             튜닝 예약
           </button>
         </Link>
 
         {/* 주차 예약 */}
         <Link href="/reserve/parking">
-          <button
-            className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
-                       bg-[#C0392B] dark:bg-[#E74C3C] dark:border-red-300 border
-                       hover:opacity-90 hover:scale-105 transition duration-300"
-          >
+          <button className="w-full px-8 py-5 text-xl font-semibold text-white rounded-2xl shadow-xl
+                            bg-[#a52c1e] dark:bg-[#E74C3C] dark:border-red-300 border
+                            hover:opacity-90 hover:scale-105 transition duration-300">
             주차 예약
           </button>
         </Link>
