8c2a48e1c80fdb54cafe26f1a8398a2f76c218b5
Wellstation <prh0917@gmail.com>
2025-07-31 17:05:51 +0900

    Update ActionButton.tsx

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -14,7 +14,7 @@ export default function ActionButton({ label, href, bgColor }: ActionButtonProps
       <Link href={href}>
         <a>
           <button
-            className={`w-72 h-16 text-xl font-bold text-yellow-300 ${bgColor}
+            className={`w-168 h-112 text-xl font-bold text-yellow-300 ${bgColor}
               rounded-full shadow-xl hover:scale-110 hover:opacity-90 transition-transform duration-300`}
           >
             {label}
