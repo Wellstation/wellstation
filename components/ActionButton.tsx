552e8a46aea57d13e4dc35229b38e031e8a81e01
Wellstation <prh0917@gmail.com>
2025-07-31 13:51:27 +0900

    Update ActionButton.tsx

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -10,15 +10,17 @@ interface ActionButtonProps {
 
 export default function ActionButton({ label, href, bgColor }: ActionButtonProps) {
   return (
-    <Link href={href}>
-      <a>
-        <button
-          className={`w-64 py-5 text-xl font-bold text-white ${bgColor} 
-            rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300`}
-        >
-          {label}
-        </button>
-      </a>
-    </Link>
+    <div className="my-3 sm:my-4">
+      <Link href={href}>
+        <a>
+          <button
+            className={`w-64 py-5 text-xl font-bold text-white ${bgColor} 
+              rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300`}
+          >
+            {label}
+          </button>
+        </a>
+      </Link>
+    </div>
   );
 }
