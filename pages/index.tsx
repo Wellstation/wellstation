8611167bba47b587d5f436c79f969a1fc3cbc643
Wellstation <prh0917@gmail.com>
2025-07-31 12:19:03 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,36 @@
 'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-between bg-white dark:bg-black px-4 py-6">
-
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black px-4 py-6">
+      
       {/* 로고 */}
-      <header className="mb-6">
+      <header className="mb-10">
         <Image src="/logo1.svg" alt="로고" width={250} height={110} />
       </header>
 
       {/* 버튼 그룹 */}
-      <main className="flex flex-col items-center gap-16 w-full max-w-xs">
+      <main className="flex flex-col items-center gap-10 mt-4">
         <Link href="/reserve/repair">
-          <button className="w-full px-8 py-6 text-2xl font-semibold text-white rounded-2xl shadow-xl
-                            bg-[#1C3058] dark:bg-[#1C3058] dark:border-gray-600 border
+          <button className="min-w-[240px] px-8 py-4 text-xl font-semibold text-white bg-[#1C385B] rounded-xl shadow-xl
                             hover:opacity-90 hover:scale-105 transition duration-300">
             정비 예약
           </button>
         </Link>
 
         <Link href="/reserve/tuning">
-          <button className="w-full px-8 py-6 text-2xl font-semibold text-white rounded-2xl shadow-xl
-                            bg-[#8444dd] dark:bg-[#A569BD] dark:border-purple-300 border
+          <button className="min-w-[240px] px-8 py-4 text-xl font-semibold text-white bg-[#8444dd] rounded-xl shadow-xl
                             hover:opacity-90 hover:scale-105 transition duration-300">
             튜닝 예약
           </button>
         </Link>
 
         <Link href="/reserve/parking">
-          <button className="w-full px-8 py-6 text-2xl font-semibold text-white rounded-2xl shadow-xl
-                            bg-[#a52c1e] dark:bg-[#E74C3C] dark:border-red-300 border
+          <button className="min-w-[240px] px-8 py-4 text-xl font-semibold text-white bg-[#a52c1e] rounded-xl shadow-xl
                             hover:opacity-90 hover:scale-105 transition duration-300">
             주차 예약
           </button>
@@ -40,7 +38,7 @@ export default function Home() {
       </main>
 
       {/* 푸터 */}
-      <footer className="mt-12 w-full">
+      <footer className="mt-20 w-full">
         <Footer />
       </footer>
     </div>
